ad78824f53ed438692abe6e7aace1aecefc00c9d
Emil Lerner <neex.emil@gmail.com>
2015-11-27 15:28:48 +0300

    add get_flag.py and set_flag.py

---
+++ scripts/get_flag.py
@@ -0,0 +1,22 @@
+import pwn, re, random, string
+
+def get_flag(ip, port, flag_id, token):
+    pwn.context(log_level='debug', timeout=10)
+    r = pwn.remote(ip, port)    
+    assert r.readline() == 'Welcome to translator!\n', "Wrong banner"
+    r.send("dict {}\n".format(flag_id))
+    info = r.readline()
+    m = re.match("Please enter your alphanumeric password xored with byte 0x([0-9A-F]{2}) and hex encoded:\n", info)
+    assert m, "Wrong password request"
+    byte = int(m.group(1), 16)
+    r.send(''.join(chr(ord(c) ^ byte) for c in token).encode('hex') + '\n')
+    assert r.readline() == 'Password ok!\n', "Can't load dictionary"
+    assert r.readline() == 'Dictionary loaded\n', "Can't load dictionary"    
+    r.send("translate secretik\n")
+    flag = r.readline().strip()
+    r.send("quit\n")
+    assert r.recvall() == "Good bye!\n", "Wrong goodbye"
+    return {"FLAG" : flag}
+
+if __name__ == "__main__":
+    print get_flag('localhost', 12343, 'RCVlzGtIjBjpPavObDWbZcMEuzYpIp', 'kdVkuKFPOidBUZSIQQqYPpdVNxZBtS')
---
+++ scripts/set_flag.py
@@ -0,0 +1,23 @@
+import pwn, re, random, string
+
+def set_flag(ip, port, flag):
+    pwn.context(log_level='debug', timeout=10)
+    flag_id = "".join(random.choice(string.ascii_letters) for i in range(30))
+    token = "".join(random.choice(string.ascii_letters) for i in range(30))    
+    r = pwn.remote(ip, port)    
+    assert r.readline() == 'Welcome to translator!\n', "Wrong banner"
+    r.send("dict {}\n".format(flag_id))
+    info = r.readline()
+    m = re.match("Please enter your alphanumeric password xored with byte 0x([0-9A-F]{2}) and hex encoded:\n", info)
+    assert m, "Wrong password request"
+    byte = int(m.group(1), 16)
+    r.send(''.join(chr(ord(c) ^ byte) for c in token).encode('hex') + '\n')
+    assert r.readline() == "Dictionary created\n", "Can't create dictionary"
+    r.send("add secretik {}\n".format(flag))
+    assert r.readline() == "Word saved!\n", "Can't store word"
+    r.send("quit\n")
+    assert r.recvall() == "Good bye!\n", "Wrong goodbye"
+    return {"FLAG_ID" : flag_id, "TOKEN" : token}
+
+if __name__ == "__main__":
+    print set_flag('localhost', 12343, 'secretflag')
